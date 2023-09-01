e9626b7b1fbc3be8a22daf7466dab0e0f7542f01
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-09-01 16:38:17 +0430

    simple hello world to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    interface Message {
+        title: string;
+        content: string;
+    }
+    const message: Message = {
+        title: "hello",
+        content: 'good Bye'
+    }
+    const changeTitle = () => {
+        message.title = 'changed'
+    }
+    $:if(message.title === 'changed'){
+        alert('title has changed')
+    }
+</script>
+<h1>{message.title}</h1>
+<p>{message.content}</p>
+
+<button on:click={changeTitle} >change the title</button>
